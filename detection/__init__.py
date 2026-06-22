3c9fff1d6db3989fcdf63a2199e0b5c7a00cc177
Chiragk352 <kulkarnichirag354@gmail.com>
2026-06-22 19:18:42 +0530

    Add public exports to detection package

--- detection/__init__.py
+++ detection/__init__.py
@@ -1,3 +1,12 @@
 from .benford_engine import compute_benford_metrics
+from .feature_engineering import build_feature_matrix
+from .model_inference import RiskScorer
+from .forensic_report import ForensicReport, ForensicReportGenerator
 
-__all__ = ["compute_benford_metrics"]
+__all__ = [
+    "compute_benford_metrics",
+    "build_feature_matrix",
+    "RiskScorer",
+    "ForensicReport",
+    "ForensicReportGenerator",
+]
